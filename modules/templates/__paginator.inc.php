d4204fb071d09ce53aaf9dd3d8085541fb582b1e
Dimik151 <dmitry_zvorigin@mail.ru>
2021-10-15 02:16:19 +0500

    add-article

--- modules/templates/__paginator.inc.php
+++ modules/templates/__paginator.inc.php
@@ -1,17 +1,19 @@
 <?php if ($paginator->page_count > 1) { ?>
-    <nav aria-label="...">
-        <ul class="pagination">
-            <?php foreach ($paginator as $number => $url) { ?>
-                <?php if ($paginator->current_page == $number) { ?>
-                    <li class="page-item active" aria-current="page">
-                        <a class="page-link" href="#"><?= $number ?></a>
-                    </li>
-                <?php } else { ?>
-                    <li class="page-item"><a class="page-link" href="<?= $url ?>"><?= $number ?></a></li>
+    <div class="container d-flex justify-content-center">
+        <nav aria-label="...">
+            <ul class="pagination">
+                <?php foreach ($paginator as $number => $url) { ?>
+                    <?php if ($paginator->current_page == $number) { ?>
+                        <li class="page-item active" aria-current="page">
+                            <a class="page-link" href="#"><?= $number ?></a>
+                        </li>
+                    <?php } else { ?>
+                        <li class="page-item"><a class="page-link" href="<?= $url ?>"><?= $number ?></a></li>
+                    <?php } ?>
                 <?php } ?>
-            <?php } ?>
-        </ul>
-    </nav>
+            </ul>
+        </nav>
+    </div>
 <?php } ?>
 
 
